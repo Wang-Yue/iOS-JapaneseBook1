91a92a0a6ffab32db6aa5716ee81e0bcc0e6c7b0
wangyue <wangyue@wangyue-macbookpro.roam.corp.google.com>
2013-10-18 11:44:53 -0700

    Initial Commit

---
+++ JBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JBook
+//
+//  Created by wangyue on 10/18/13.
+//  Copyright (c) 2013 wangyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JBook
+//
+//  Created by wangyue on 10/18/13.
+//  Copyright (c) 2013 wangyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
